2e9bb8aeba85f49f0197057783f8cce9a58f2af6
Addison Bassock <addie@Addisons-MacBook-Pro.local>
2020-11-27 23:25:29 -0500

    making the requests

--- SpotifyAPI_info.py
+++ SpotifyAPI_info.py
@@ -1,1 +1,14 @@
+import sqlite3
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# the url to read from
+url = "https://spotifycharts.com/regional/global/daily/2020-11-21"
+
+resp = requests.get(url)
+soup = BeautifulSoup(resp.content, 'html.parser')
+
+# Dictionary of Top 200 Songs from 11-21-2020
+top_200 = {}
 
