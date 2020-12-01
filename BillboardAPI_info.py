34cb6171278dc0693bc29f91396f318fcaba6533
Sarah Stupak <your_email@youremail.com>
2020-12-01 16:13:44 -0500

    with functions but not 25

--- BillboardAPI_info.py
+++ BillboardAPI_info.py
@@ -4,56 +4,55 @@ import sqlite3
 import os 
 import json
 
-#def billboard_lists():
-c = billboard.ChartData('hot-100', date = '2020-11-21', year = None, fetch = True, timeout = 25)
-title_lst = []
-artist_lst = []
-rank_lst = []
-for rank in c:
-    t = rank.title
-    a = rank.artist
-    r = rank.rank
-    rank_lst.append(r)
-    title_lst.append(t)
-    artist_lst.append(a)
-#return ((title_lst, artist_lst, rank_lst))
+def db_setup(db_name):
+    dir = os.path.dirname(__file__) + os.sep
+    conn = sqlite3.connect(dir + 'Billboard.db')
+    cur = conn.cursor()
+    #cur.execute('''DROP TABLE IF EXISTS Billboard''')
+    return cur, conn
 
+def get_data():
+    c = billboard.ChartData('hot-100', date = '2020-11-21', year = None, fetch = True, timeout = 25)
+    return c
 
-#def db_setup(cur, conn):
-dir = os.path.dirname(__file__) + os.sep
-conn = sqlite3.connect(dir + 'Billboard.db')
-cur = conn.cursor()
-#cur.execute('''DROP TABLE IF EXISTS Billboard''')
-cur.execute('''CREATE TABLE IF NOT EXISTS Billboard (title TEXT, artist TEXT, rank INTEGER)''')
-    
-
-#def add_to_db(cur, conn):
-count = 0 
+def create_database():
+    title_lst = []
+    artist_lst = []
+    rank_lst = []
+    c = get_data()
+    for rank in c:
+        t = rank.title
+        a = rank.artist
+        r = rank.rank
+        rank_lst.append(r)
+        title_lst.append(t)
+        artist_lst.append(a)
+    cur, conn = db_setup('Billboard.db')
+    cur.execute('''CREATE TABLE IF NOT EXISTS Billboard (title TEXT, artist TEXT, rank INTEGER)''')
+    count = 0 
     #t = []
     #for l in billboard_lists():
         #t.append(l[0])
-for i in range(len(title_lst)):
-    if count > 25:
-        print('Retrieved 25 songs, restart to retrieve more')
-        break 
-    try:
-        cur.execute('INSERT INTO Billboard (title, artist, rank) VALUES (?, ?, ?)', (title_lst[i], artist_lst[i], rank_lst[i]))
+    for i in range(len(title_lst)):
+        if count > 25:
+            print('Retrieved 25 songs, restart to retrieve more')
+            break 
+        try:
+            cur.execute('INSERT INTO Billboard (title, artist, rank) VALUES (?, ?, ?)', (title_lst[i], artist_lst[i], rank_lst[i]))
             #data = cur.fetchone()[0]
             #print('Found in database ', title)
             #continue
-    except:
-        pass
-    count += 1
-conn.commit()
+        except:
+            pass
+        count += 1
+    conn.commit()
 
-#def main():
-    #billboard_lists()
-    #db_setup(cur, conn)
-    #add_to_db(cur, conn)
+def main():
+    create_database()
 
 
-#if __name__ == "__main__":
-    #main()
+if __name__ == "__main__":
+    main()
 
 #cur.execute('SELECT title FROM Billboard')
 #lst = cur.fetchall()
