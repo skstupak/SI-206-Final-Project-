cb30a0a52dd50d13309cfa15d70e58f355efced4
Addison Bassock <addie@Addisons-MacBook-Pro.local>
2020-12-01 21:40:33 -0500

    Added new songs

--- SpotifyAPI_info.py
+++ SpotifyAPI_info.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import billboard
 
 # the url to read from
-url = "https://spotifycharts.com/regional/global/daily/2020-11-21"
+url = "https://spotifycharts.com/regional/us/daily/2020-11-21"
 def GetSoupObject(url):
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, 'html.parser')
@@ -23,6 +23,31 @@ def ScrapeSpotify(soup):
     # Create the variable track_name
         track = song.find('td', class_='chart-table-track')
         track_name = track.strong.text
+        if " (feat." in track_name:
+            fixingsong = track_name.split("(feat.")
+            fixed_song = fixingsong[0].strip()
+            track_name = fixed_song
+        if " (Feat." in track_name:
+            fixingsong = track_name.split("(Feat.")
+            fixed_song = fixingsong[0].strip()
+            track_name = fixed_song
+        if " (with" in track_name:
+            fixingsong = track_name.split("(with")
+            fixed_song = fixingsong[0].strip()
+            track_name = fixed_song
+        if " (" in track_name:
+            fixingsong = track_name.split("(")
+            fixed_song = fixingsong[0].strip()
+            track_name = fixed_song
+        if " -" in track_name:
+            fixingsong = track_name.split(" -")
+            fixed_song = fixingsong[0].strip()
+            track_name = fixed_song
+        if " [" in track_name:
+            fixingsong = track_name.split(" [")
+            fixed_song = fixingsong[0].strip()
+            track_name = fixed_song
+        track_name = track_name.title()
     # Create the variables artist_name, position_number, and streams_number 
         artist = song.find('td', class_='chart-table-track')
         artist_name = artist.span.text.strip('by ')
@@ -36,30 +61,6 @@ def ScrapeSpotify(soup):
 
 spotify_dict = ScrapeSpotify(soup)
 
-def FixSongs(spotify_dict):
-    for song in spotify_dict.keys():
-        if " (feat." in song:
-            fixingsong = song.split("(feat.")
-            fixed_song = fixingsong[0].strip()
-            song = fixed_song
-        if " (Feat." in song:
-            fixingsong = song.split("(Feat.")
-            fixed_song = fixingsong[0].strip()
-            song = fixed_song
-        if " (with" in song:
-            fixingsong = song.split("(with")
-            fixed_song = fixingsong[0].strip()
-            song = fixed_song
-        if " (" in song:
-            fixingsong = song.split("(")
-            fixed_song = fixingsong[0].strip()
-            song = fixed_song
-        song = song.title()
-    return spotify_dict
-
-fixed_dict = FixSongs(spotify_dict)
-spotify_keys = fixed_dict.keys()      
-
 # Loop through songs in Billboard database and create dictionary of the Spotify songs that are also in this database
 dir = os.path.dirname(__file__) + os.sep
 conn = sqlite3.connect(dir + 'Billboard.db')
@@ -71,11 +72,15 @@ titles = cur.fetchall()
 cur.execute('''DROP TABLE IF EXISTS Spotify''')
 cur.execute('''CREATE TABLE Spotify (title TEXT, artist TEXT, position INTEGER, streams INTEGER)''')
 
+cur.execute("""INSERT INTO Spotify (title, artist, position, streams) VALUES ('pov', 'Ariana Grande', 34, 2046473)""")
+cur.execute("""INSERT INTO Spotify (title, artist, position, streams) VALUES ('ily', 'Surf Mesa', 135, 927563)""")
+cur.execute("""INSERT INTO Spotify (title, artist, position, streams) VALUES ('WAP', 'Cardi B', 8, 864973)""")
+cur.execute("""INSERT INTO Spotify (title, artist, position, streams) VALUES ('Savage Love', 'Jawsh 685, Jason Derulo', 147, 267632)""")
 
 for title in titles:
     title = title[0]
-    if title in spotify_keys:
-        cur.execute('INSERT INTO Spotify (title, artist, position, streams) VALUES (?, ?, ?, ?)', (title, fixed_dict[title][0], fixed_dict[title][1], fixed_dict[title][2]))
+    if title in spotify_dict.keys():
+        cur.execute('INSERT INTO Spotify (title, artist, position, streams) VALUES (?, ?, ?, ?)', (title, spotify_dict[title][0], spotify_dict[title][1], spotify_dict[title][2]))
 conn.commit()
         
 
