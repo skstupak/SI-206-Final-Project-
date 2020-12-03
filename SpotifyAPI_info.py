23bf2ed53c9d200988a065a4e2eb97c7b9177f7c
Addison Bassock <addie@Addisons-MacBook-Pro.local>
2020-12-02 19:43:17 -0500

    Changed date

--- SpotifyAPI_info.py
+++ SpotifyAPI_info.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import billboard
 
 # the url to read from
-url = "https://spotifycharts.com/regional/us/daily/2020-04-05"
+url = "https://spotifycharts.com/regional/us/daily/2020-12-01"
 def GetSoupObject(url):
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, 'html.parser')
